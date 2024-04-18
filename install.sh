e38d910db75a0aac26b8391e7e8e507985c534fb
Archer Heffern <heffernarcher@gmail.com>
2024-04-18 01:21:54 -0400

    tmux conf and install script update

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
-read -p "Warning! This will override your current vimrc. Proceed? (y/n)" CONTINUE
-if [ $CONTINUE = "y" ]
-then 
-  cp -r ./colors ~/.vim/colors
-  cp .vimrc ~/.vimrc
+if [ -e ~/.vimrc ]; then
+	read -p "Warning! This will override your current vimrc. Proceed? (y/n)" CONTINUE
+else
+	CONTINUE=y
 fi
+
+if [ $CONTINUE != 'y' ]; then
+	echo "Passing..."
+else
+	cp -r ./colors ~/.vim/colors
+	cp .vimrc ~/.vimrc
+fi
+
+if [ -e ~/.tmux.conf ]; then
+	read -p "Warning! This will override your current tmux.conf. Proceed? (y/n)" CONTINUE
+else
+	CONTINUE=y
+fi
+
+if [ $CONTINUE != 'y' ]; then
+	echo "Passing..."
+else
+	cp .tmux.conf ~/tmux.conf
+fi
+echo "Done"
