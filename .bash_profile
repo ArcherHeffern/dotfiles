79334f00879d563de5640f86219e74c65f8956e5
Archer Heffern <hefferna@brandeis.edu>
2025-03-03 00:50:01 +0000

    removed many errors and made easier to use

--- .bash_profile
+++ .bash_profile
@@ -1,3 +1,5 @@
+echo "Running bash_profile"
+
 alias la='ls -la'
 alias ls='ls -F'
 alias haskell-language-server='haskell-language-server-9.10'
@@ -9,10 +11,20 @@ alias gl='git log --all --decorate --oneline --graph'
 alias indent='gindent -kr -i4'
 alias mv='mv -n'
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
 eval "$(zoxide init bash)"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
-source "$HOME/.cargo/env"
+CARGO_ENV="$HOME/.cargo/env"
+if [[ -e $CARGO_ENV ]]; then
+	source $CARGO_ENV
+fi
+
+EMDSK_ENV="/Users/archerheffern/Desktop/cloned/emsdk/emsdk_env.sh"
+if [[ -f $EMSDK_ENV ]]; then
+	source $EMSDK_ENV
+fi
 
 PS1='\w \$ '
 
@@ -28,9 +40,9 @@ export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="/usr/libexec/:$PATH"
 export PATH="~/.ghcup/bin/:$PATH"
 export PATH="~/Desktop/code/brandeis_purity_test/scripts:$PATH"
+export PATH="~/.local/bin:$PATH"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export PATH="/opt/homebrew/opt/llvm@17/bin:$PATH"
 
 export CC="/opt/homebrew/Cellar/gcc/14.2.0_1/bin/gcc-14"
 export CXX="/opt/homebrew/Cellar/gcc/14.2.0_1/bin/g++-14"
-source "/Users/archerheffern/Desktop/cloned/emsdk/emsdk_env.sh"
-export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
-export PATH="/opt/homebrew/opt/llvm@17/bin:$PATH"
--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+# zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh > /dev/null
+
+# Vundle
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi 
+
 # VIMRC
 if [ -e ~/.vimrc ]; then
-	read -p "Warning! This will override your current vimrc. Proceed? (y/n)" CONTINUE
+	read -p "Warning! This will override your current vimrc. Proceed? (y/n) " CONTINUE
 else
 	CONTINUE=y
 fi
@@ -8,13 +16,14 @@ fi
 if [ $CONTINUE != 'y' ]; then
 	echo "Passing..."
 else
+	mkdir -p ~/.vim/
 	cp -r ./colors ~/.vim/colors
 	cp .vimrc ~/.vimrc
 fi
 
 # BASH_PROFILE
 if [ -e ~/.bash_profile ]; then
-	read -p "Warning! This will override your current bash_profile. Proceed? (y/n)" CONTINUE
+	read -p "Warning! This will override your current bash_profile. Proceed? (y/n) " CONTINUE
 else
 	CONTINUE=y
 fi
@@ -24,11 +33,10 @@ if [ $CONTINUE != 'y' ]; then
 else
 	cp .bash_profile ~/.bash_profile
 fi
-echo "Done"
 
 # TMUX
 if [ -e ~/.tmux.conf ]; then
-	read -p "Warning! This will override your current tmux.conf. Proceed? (y/n)" CONTINUE
+	read -p "Warning! This will override your current tmux.conf. Proceed? (y/n) " CONTINUE
 else
 	CONTINUE=y
 fi
@@ -38,4 +46,7 @@ if [ $CONTINUE != 'y' ]; then
 else
 	cp .tmux.conf ~/.tmux.conf
 fi
-echo "Done"
+
+echo 'Done. Next:'
+echo 'Launch vim and run :PluginInstall'
+echo 'Source the new bash_profile using `source ~/.bash_profile`'
