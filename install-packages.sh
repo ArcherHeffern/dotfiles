84580892fde49840493faa28647b9007aedcebac
ArcherHeffern <heffernarcher@gmail.com>
2025-07-21 22:39:45 -0700

    remove ,

--- install-packages.sh
+++ install-packages.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 CROSS_PLATFORM_PACKAGES=('sl' 'bat' 'tree' 'jq' 'fzf' 'cloc' 'ffmpeg' 'imagemagick' 'yt-dlp' 'neofetch')
-MACOS_PACKAGES=('fd', 'grep', 'openjdk')
+MACOS_PACKAGES=('fd' 'grep' 'openjdk')
 LINUX_PACKAGES=('fd-find' 'net-tools' 'zip' 'zoxide' 'default-jre' 'default-jdk' 'presenterm')
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
